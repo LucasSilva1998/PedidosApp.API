7ae1028d22bcde639a883478d06cf239192e2426
Lucas Silva <Lucas Silva@FIO32001701>
2025-05-20 16:32:35 -0300

    Adicionar arquivos de projeto.

---
+++ PedidosApp.API/Entities/Pedido.cs
@@ -0,0 +1,14 @@
+﻿namespace PedidosApp.API.Entities
+{
+    /// <summary>
+    /// Modelo de entidade para Pedido
+    /// </summary>
+    public class Pedido
+    {
+        public Guid? Id { get; set; }
+        public DateTime? DataHora { get; set; }
+        public decimal Valor { get; set; }
+        public string? Cliente { get; set; }
+        public string? Detalhes { get; set; }
+    }
+}
